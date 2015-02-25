ac6132fdec948e729cce4893008bd2ac2e48483d
Kawant's <Kawant's@10.102.92.124>
2015-02-25 10:33:04 -0600

    Find PI

---
+++ Assignment 5/src/FindPI.java
@@ -0,0 +1,22 @@
+
+public class FindPI 
+{
+	public static void main(String[] args)
+	{
+		int count = 999999999;
+	    double pi = 0;
+	    double denominator = 1;
+	 
+	    for (int x = 0; x < count; x++) 
+	    {
+	    	if (x % 2 == 0) 
+	    		pi = pi + (1 / denominator);
+	    	else 
+	            pi = pi - (1 / denominator);
+	    	
+	    	denominator += 2;
+	    }
+	    pi *= 4;
+	    System.out.println(pi);
+	}
+}
